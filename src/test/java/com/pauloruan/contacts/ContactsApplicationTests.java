c84e46ce7ee70b5796cf8ff1f7e5f8eeca761f43
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-04-08 17:06:57 -0300

    chore: Project started

---
+++ src/test/java/com/pauloruan/contacts/ContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pauloruan.contacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
